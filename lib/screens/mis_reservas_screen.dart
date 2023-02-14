709650eeade40e6063e1e76d58535aeac7e006e5
Tristan-Al <tristanalonso09@gmail.com>
2023-02-14 12:43:06 +0100

    creada screen mis reservas

---
+++ lib/screens/mis_reservas_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MisReservasScreen extends StatelessWidget {
+   
+  const MisReservasScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('MisReservasScreen'),
+      ),
+    );
+  }
+}
