aa6e2f843175f1c01c365175559a3ff95d55bb4d
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-02-16 20:10:13 +0100

    Register v2

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppTheme{
+class AppTheme {
   static const Color mainTextColor = Color.fromRGBO(255, 255, 255, 1);
   static const Color secondaryTextColor = Color.fromRGBO(0, 0, 0, 1);
   static const Color mainColor = Color(0xFFECEFF1);
@@ -8,24 +8,21 @@ class AppTheme{
   static const Color buttomColor = Color(0xFFAA00FF);
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-
     primaryColor: mainColor,
-
     appBarTheme: const AppBarTheme(
       color: mainColor,
       elevation: 3,
     ),
-
     inputDecorationTheme: const InputDecorationTheme(
-      floatingLabelStyle: TextStyle(color: mainColor),
+      floatingLabelStyle: TextStyle(color: secondary),
       //Habilitado
       enabledBorder: OutlineInputBorder(
-        //borderSide: BorderSide(color: primary),
-        //borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
+          //borderSide: BorderSide(color: primary),
+          //borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
       //Foco
       focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: mainColor),
+        borderSide: BorderSide(color: secondary),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
       //Generico
@@ -33,7 +30,5 @@ class AppTheme{
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
     ),
-
-    
   );
 }
