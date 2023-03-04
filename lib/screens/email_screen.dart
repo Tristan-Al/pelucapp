e0fbf5926f01eb3e6082f717b395ff508a14b893
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-03-04 22:02:42 +0100

    Correcciones recuperacion email

--- lib/screens/email_screen.dart
+++ lib/screens/email_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pelucapp/screens/screens.dart';
-import 'package:pelucapp/theme/app_theme.dart';
 import 'package:pelucapp/widgets/widgets.dart';
 
 class EmailScreen extends StatelessWidget {
@@ -44,48 +42,36 @@ class _FormLogIn extends StatelessWidget {
             IconButton(
               alignment: Alignment.topLeft,
               onPressed: () => {Navigator.pop(context)},
-              icon: Icon(Icons.arrow_back_ios, size: 28, color: Colors.black),
+              icon: const Icon(Icons.arrow_back_ios,
+                  size: 28, color: Colors.black),
             ),
           ],
         ),
         const SizedBox(height: 30),
-        Text('¿Has olvidado tu contraseña?',
+        const Text('¿Has olvidado tu contraseña?',
             textAlign: TextAlign.left,
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
         const SizedBox(height: 30),
-        Text(
+        const Text(
           'Te enviaremos las instrucciones a tu e-mail para que puedas restablecerla',
           textAlign: TextAlign.left,
         ),
         const SizedBox(height: 30),
-        TextFormField(
-          autocorrect: false,
+        FormEmail(
+          ocultar: false,
+          hintText: 'Email',
           keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(
-              hintText: 'john.doe@gmail.com',
-              labelText: 'E-mail',
-              suffixIcon: Icon(Icons.key),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ))),
-          onChanged: (value) => formValues[formProperty] = value,
-          validator: (value) {
-            String pattern =
-                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-            RegExp regExp = RegExp(pattern);
-
-            return regExp.hasMatch(value ?? '')
-                ? null
-                : 'El valor ingresado no luce como un correo';
-          },
+          icon: Icons.email_outlined,
+          formProperty: 'email',
+          formValues: formValues,
         ),
         const SizedBox(height: 55),
         Container(
           padding: const EdgeInsets.only(left: 20, right: 20),
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, 'login');
+            },
             child: const Text('Restablecer contraseña',
                 style: TextStyle(fontSize: 20)),
           ),
