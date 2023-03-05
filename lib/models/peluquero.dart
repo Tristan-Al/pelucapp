ced1784f1a21329e3379af1a8d971a52824058f9
Tristan-Al <tristanalonso09@gmail.com>
2023-03-05 12:05:40 +0100

    Modificacion peluquero

--- lib/models/peluquero.dart
+++ lib/models/peluquero.dart
@@ -4,18 +4,24 @@
 
 import 'dart:convert';
 
+import 'package:pelucapp/models/models.dart';
+
 class Peluquero {
     Peluquero({
         required this.atiende,
+        required this.horario,
         this.imagen,
         required this.nombre,
+        required this.servicios,
         required this.telefono,
     });
 
     String? id;
     String atiende;
+    Map<String, bool> horario;
     String? imagen;
     String nombre;
+    Map<String, Servicio> servicios;
     int telefono;
 
     factory Peluquero.fromJson(String str) => Peluquero.fromMap(json.decode(str));
@@ -24,15 +30,19 @@ class Peluquero {
 
     factory Peluquero.fromMap(Map<String, dynamic> json) => Peluquero(
         atiende: json["atiende"],
+        horario: Map.from(json["horario"]).map((k, v) => MapEntry<String, bool>(k, v)),
         imagen: json["imagen"],
         nombre: json["nombre"],
+        servicios: Map.from(json["servicios"]).map((k, v) => MapEntry<String, Servicio>(k, Servicio.fromMap(v))),
         telefono: json["telefono"],
     );
 
     Map<String, dynamic> toMap() => {
         "atiende": atiende,
+        "horario": Map.from(horario).map((k, v) => MapEntry<String, dynamic>(k, v)),
         "imagen": imagen,
         "nombre": nombre,
+        "servicios": Map.from(servicios).map((k, v) => MapEntry<String, dynamic>(k, v.toMap())),
         "telefono": telefono,
     };
 }
--- lib/widgets/small_text.dart
+++ lib/widgets/small_text.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pelucapp/theme/app_theme.dart';
 
 class SmallText extends StatelessWidget {
+  TextAlign? align;
   Color? color;
   final String text;
   double size;
@@ -11,13 +12,15 @@ class SmallText extends StatelessWidget {
     this.color = AppTheme.mainTextColor,
     required this.text,
     this.size = 20,
-    this.height = 1.2
+    this.height = 1.2,
+    this.align = TextAlign.start
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: align,
       overflow: TextOverflow.clip,
       style: TextStyle(
         color: color,
