4ad6df70c83aa2e6e931d0a12b11ce845e0ab95f
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-03-06 14:58:30 +0100

    posible solucion

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'package:pelucapp/widgets/big_text.dart';
 export 'package:pelucapp/widgets/small_text.dart';
 export 'package:pelucapp/widgets/form_field.dart';
 export 'package:pelucapp/widgets/form_email.dart';
+export 'package:pelucapp/widgets/form_field_editar.dart';
